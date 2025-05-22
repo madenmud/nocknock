849e365fd76f73e88bb1d1ff9415419f6b64c307
madenmud <madenmud@gmail.com>
2025-05-22 02:45:43 +0000

    use net

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 docker run -it --rm \
+  -v $(pwd)/data:/nockchain/data \
   -v $(pwd)/wallet-data:/nockchain/data/wallet \
   -v $(pwd)/scripts:/nockchain/scripts \
+  -v $(pwd)/.env:/nockchain/.env \
   --name nockchain \
+  --net vlan \
   madenmud/nockchain-node:latest

bb883d209188fd7afd66a02647d3ff5ce949881e
madenmud <madenmud@gmail.com>
2025-05-22 13:08:49 +0000

    script updated

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@ docker run -it --rm \
   -v $(pwd)/data:/nockchain/data \
   -v $(pwd)/wallet-data:/nockchain/data/wallet \
   -v $(pwd)/scripts:/nockchain/scripts \
-  --net=ipvlan \
+  -p 3006:3006 -p 3006:3006/udp \
   madenmud/nockchain-node:latest
   #-v $(pwd)/.env:/nockchain/.env \
   #-p 3005:3005 -p 3006:3006 \
   #--name nockchain \
+  # --net=macvlan \
+  #--privileged \
+  #--net=ipvlan \
+  #--ip=192.168.0.3 \
--- scripts/run_node_env_key.sh
+++ scripts/run_node_env_key.sh
@@ -1,3 +1,18 @@
 export $(grep -v '^#' .env | xargs)
 echo $MINING_PUBKEY
-nockchain --mining-pubkey $MINING_PUBKEY --mine 
+
+
+
+# Define environment variables
+PEERS="/ip4/95.216.102.60/udp/3006/quic-v1 /ip4/65.108.123.225/udp/3006/quic-v1 /ip4/65.109.156.108/udp/3006/quic-v1 /ip4/65.21.67.175/udp/3006/quic-v1 /ip4/65.109.156.172/udp/3006/quic-v1 /ip4/34.174.22.166/udp/3006/quic-v1 /ip4/34.95.155.151/udp/30000/quic-v1 /ip4/34.18.98.38/udp/30000/quic-v1"
+MAX_INCOMING=1000
+MAX_OUTGOING=1000
+MAX_ESTABLISHED=2000
+MAX_PER_PEER=10
+
+# Export variables to make them available to the command
+export PEERS MAX_INCOMING MAX_OUTGOING MAX_ESTABLISHED MAX_PER_PEER
+
+# Execute the nockchain command
+rm -f "$NPC_SOCKET" && /root/.cargo/bin/nockchain --npc-socket "$NPC_SOCKET" --mining-pubkey "$MINING_PUBKEY" --bind /ip4/0.0.0.0/udp/"$BIND_PORT"/quic-v1 --mine --peer `echo $PEERS | sed 's/ / --peer /g'` --max-established-incoming "$MAX_INCOMING" --max-established-outgoing "$MAX_OUTGOING" --max-established "$MAX_ESTABLISHED" --max-established-per-peer "$MAX_PER_PEER"
+
